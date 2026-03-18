31b4f04a06ea6427a3454d4aeea774ce431654db
nikki-nandi <nikithnandi08@gmail.com>
2026-03-18 14:56:40 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASE_URL = "sqlite:///./tickets.db"
 
-DB_PATH = os.path.join(BASE_DIR, "tickets.db")
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-DATABASE_URL = f"sqlite:///{DB_PATH}"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(bind=engine)
