8c7ac305d35b42081c6763b4291d7d659d3e2b2f
nikki-nandi <nikithnandi08@gmail.com>
2026-03-18 14:57:13 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,21 +1,14 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-
 class Ticket(Base):
-
     __tablename__ = "tickets"
 
-    id = Column(Integer, primary_key=True, index=True)
-
+    id = Column(Integer, primary_key=True)
     ticket_id = Column(String, unique=True)
-
     name = Column(String)
-
     email = Column(String)
-
     reason = Column(String)
-
     status = Column(String, default="Open")
